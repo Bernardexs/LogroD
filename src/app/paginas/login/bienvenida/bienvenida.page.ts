b7fc97b6e4461984f0e27acd24c63b0d4b3320d3
Yuseydi <pamelacamisan26@gmail.com>
2024-08-11 23:06:50 -0500

    arreglos en css

--- src/app/paginas/login/bienvenida/bienvenida.page.ts
+++ src/app/paginas/login/bienvenida/bienvenida.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+Router
 @Component({
   selector: 'app-bienvenida',
   templateUrl: './bienvenida.page.html',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BienvenidaPage implements OnInit {
 
-  constructor() { }
+  constructor(public router:Router) { 
+    setTimeout(()=>{
+      this.router.navigateByUrl('iniciar-sesion')
+    },4000)
+  }
 
   ngOnInit() {
   }
